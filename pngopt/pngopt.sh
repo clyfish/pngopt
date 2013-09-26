b8116e31141f73c3c1c2502b4f14c602e745c37b
Cheng Liangyu <clyfish@gmail.com>
2013-09-26 13:38:39 +0800

    add deflopt and defluff

--- pngopt/pngopt.sh
+++ pngopt/pngopt.sh
@@ -6,24 +6,26 @@ BIN=`dirname $0`
 for png in "$@"
 do
     echo -n "$png	"
-    pre_size=`stat -c"%s" $png`
+    pre_size=`stat -c"%s" "$png"`
     optipng_size=-1
     pngout_size=-1
     advdef_size=-1
     while :
     do
-        [[ "`stat -c"%s" $png`" -eq "$optipng_size" ]] && break
+        [[ "`stat -c"%s" "$png"`" -eq "$optipng_size" ]] && break
         $BIN/optipng -o7 -q "$png"
-        optipng_size="`stat -c"%s" $png`"
+        optipng_size="`stat -c"%s" "$png"`"
 
-        [[ "`stat -c"%s" $png`" -eq "$pngout_size" ]] && break
-        $BIN/pngout "$png" -q -y -knpTc
-        pngout_size="`stat -c"%s" $png`"
+        [[ "`stat -c"%s" "$png"`" -eq "$pngout_size" ]] && break
+        $BIN/pngout "$png" -q -y -knpTc,npLb
+        pngout_size="`stat -c"%s" "$png"`"
 
-        [[ "`stat -c"%s" $png`" -eq "$advdef_size" ]] && break
+        [[ "`stat -c"%s" "$png"`" -eq "$advdef_size" ]] && break
         $BIN/advdef -z4 "$png" >/dev/null
-        advdef_size="`stat -c"%s" $png`"
+        advdef_size="`stat -c"%s" "$png"`"
     done
+    $BIN/defluff < "$png" 2>/dev/null > ".$png.tmp.$$" && mv ".$png.tmp.$$".mv "$png"
+    $BIN/deflopt -s "$png"
     post_size=`stat -c"%s" $png`
     echo "$pre_size => $post_size"
 done
